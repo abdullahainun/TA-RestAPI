c775c85b099d0e7dbfda15329b810393928f26c5
Ainun abdullah <abdullah.ainun4@gmail.com>
2018-12-27 08:52:38 +0700

    change to 9999

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://localhost/spark');
 
 var db = mongoose.connection;
 // Setup server port
-var port = process.env.PORT || 8888;
+var port = process.env.PORT || 9999;
 
 
 // Send message for default URL
