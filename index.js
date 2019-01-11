84ad75d86be594d2be7ea1b15367f918712a0101
Ainun abdullah <abdullah.ainun4@gmail.com>
2019-01-11 13:17:05 +0700

    add

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ mongoose.connect('mongodb://localhost/spark');
 
 var db = mongoose.connection;
 // Setup server port
-var port = process.env.PORT || 6789;
+var port = process.env.PORT || 80;
 
 
 // Send message for default URL
